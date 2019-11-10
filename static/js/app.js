d9b17e7e7f8d23c10b70c10db593958e3be34c11
prairir <me@ryanprairie.com>
2019-11-10 18:48:35 -0500

    ok i swear i tested it this time

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,1 @@
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
 particlesJS.load('particles-js','../static/js/particles.json', console.log('sup'));
-
-var ctx = document.getElementsByTagName('canvas')[0];
-ctx.width = x;
-ctx.height =y;
