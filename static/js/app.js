28b2b909066774d58982f751c4c2ef58e7249eae
prairir <me@ryanprairie.com>
2019-11-10 18:06:22 -0500

    hopefully it worked this time

--- static/js/app.js
+++ static/js/app.js
@@ -5,8 +5,8 @@ var w = window,
     x = w.innerWidth || e.clientWidth || g.clientWidth,
     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-canvas = document.getElementByClassName('particles-js-canvas-el');
+canvas = document.getElementsByClassName('particles-js-canvas-el')[0];
 canvas.setAttribute('width', x);
 canvas.setAttribute('height', y);
 
-particlesJS.load('particles-js','../static/js/particles.json', console.log('sup'));
+particlesJS.load('particles-js','../static/js/particles.json', console.log('hi'));
