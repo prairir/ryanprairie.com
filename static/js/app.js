d6259dce87b2468d19180225b70d2270f5d627bd
prairir <me@ryanprairie.com>
2019-11-10 18:03:28 -0500

    hopefully fixed the canvas issues

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,12 @@
-particlesJS.load('particles-js','static/js/particles.json', console.log('sup'));
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+canvas = document.getElementByClassName('particles-js-canvas-el');
+canvas.setAttribute('width', x);
+canvas.setAttribute('height', y);
+
+particlesJS.load('particles-js','../static/js/particles.json', console.log('sup'));
